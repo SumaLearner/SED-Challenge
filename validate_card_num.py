c072e3c77e9c7ba99a361e0d31eff87a20f0fe02
SumaLearner <32079946+SumaLearner@users.noreply.github.com>
2019-03-20 13:39:11 -0400

    Update validate_card_num.py

--- validate_card_num.py
+++ validate_card_num.py
@@ -1,16 +1,16 @@
-import itertools
+from itertools import groupby
 import re
 
 pattern1 = r'([456]\d{3}(-?\d{4}){3}$|\d{16}'
 card_num = '5644-5567-7878-1121'
 
-def repetition(card_num):
-    return max(len(list(g)) for _, g in groupby(card_num)
+#def repetition(card_num):
+#    return max(len(list(g)) for _, g in groupby(card_num)
 
 if re.match( pattern1, card_num):
     print("hyphen and string length are correct")
     num = card_num.replace('-', '')
-    if repetition(card_num.replace('-', '') >= 4:
+    if max(len(list(g)) for _, g in groupby(num)) >= 4:
        print('more than 4 repetiotions... number Invalid')
     else:
        print('Valid')
